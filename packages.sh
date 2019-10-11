ef2fa5b1cf7081de5a8d494599af6ba2e9deb035
Andy Fessel <38083206+andyfessel@users.noreply.github.com>
2019-10-10 18:48:31 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+if [ -e /usr/bin/wget ]; then
+  exit 0
+fi
 yum -y install wget
+wget https://raw.githubudercontent.nic-instructions/hello-nti-300/master/packages.txt
 for packages in $(cat packages.txt); do
   yum -y install $packages
 done  
