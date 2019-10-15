09cb9eaec237c1b80db72d4b5c97c906d833f175
Andy Fessel <38083206+andyfessel@users.noreply.github.com>
2019-10-14 20:36:14 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-if [ -e /usr/bin/wget ]; then
-  exit 0
-fi
 yum -y install wget
 wget https://raw.githubusercontent.com/andyfessel/NTI-300/master/packages.txt
 for packages in $(cat packages.txt); do
